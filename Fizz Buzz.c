e9cc1a6be1164501b5d7d6749aa6e9f39e271fde
arthurtxss <arthurtxss@gmail.com>
2025-03-27 23:28:47 -0300

    Fizz Buzz (3 e 5).c
    O objetivo da atividade era, quando o número fosse múltiplo de 3, a palavra “Fizz” tomaria o lugar do número e o mesmo deveria acontecer com os números múltiplos de 5, só que dessa vez o número seria trocado pela palavra “Buzz”.
    Há ainda uma terceira situação, que ocorre quando o número é múltiplo de 3 e 5, quando isso acontece, o número deve ser substituído por “FizzBuzz”, e quando o número não está em nenhuma dessas condições, ele será lido normalmente.
    Um exemplo seria	:
    1
    2
    Fizz (3)
    4
    Buzz (5)
    Fizz (6)
    7
    8
    Fizz (9)
    Buzz (10)
    11
    Fizz (12)
    13
    14
    FizzBuzz (15)
    
    Para saber se o número era múltiplo, foi usado o conceito de restante da divisão com “%” (ele irá deixar apenas o restante da divisão dos dois números), um exemplo é o próprio número 3, onde 3%3=0 (resto zero),nesse sentido todo número que tiver resto “0”, é considerado um múltiplo de três, esse raciocínio foi seguido para os múltiplos de 5 também.

--- Fizz Buzz.c
+++ Fizz Buzz.c
@@ -1,15 +1,21 @@
-#include <stdio.h>
+ #include <stdio.h>
 
 int main() {
 int i;
-for (i=1;i<=100;i++)
-if(i%3==0){
-    printf("fizz\n");
-
+for (i=1;i<=50;i++)
+ 
+if(i%3==0&&i%5==0){
+    printf("FizzBuzz\n");
+ }
+   else if(i%3==0){
+    printf("Fizz\n");
+    }
+else if(i%5==0){
+    printf("Buzz\n");
 }
+
 else
 printf("%d\n",i);
 
-
-    return 0;
+    return (0);
 }
